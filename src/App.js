75b3040847fcea632975b172a2dde01dfc2bdee5
Olya Yevtyunina <olyayevtyunina@Olyas-MacBook-Pro.local>
2022-11-30 18:01:34 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { data } from './data';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [portrait, setPortrait] = useState(0);
+  const {id, name, description, image} = data[portrait];
+
+  const previousPortrait = () =>{
+    setPortrait((portrait =>{
+      portrait --;
+      if (portrait < 0){
+      return data.length - 1;
+      }
+      return portrait;
+    }))
+  }
+
+  const nextPortrait = () =>{
+    setPortrait((portrait =>{
+      portrait++;
+      if (portrait > data.length - 1){
+        portrait = 0;
+      }
+      return portrait;
+    }))
+  }
+
+  return(
+  <div>
+  <div className="container">
+  <h1>Best Cities to live in turkey</h1>
+  </div>
+
+  <div className="container">
+  <h3>{id} - {name}</h3>
+  </div>
+
+  <div className="container">
+  <button onClick={previousPortrait}>Prev</button>
+  <img src={image} width="800" height="500vh" alt="turkey"/>
+  <button onClick={nextPortrait}>Next</button>
+  </div>
+
+  <div className="container">
+  <h2>{description}</h2>
+  </div>
+  </div>
+  )
 }
 
 export default App;
+
---
+++ src/data.js
@@ -0,0 +1,59 @@
+export const data = [
+
+    {
+    id: 1,
+    name: "Istanbul",
+    description: "Istanbul, Turkish İstanbul, formerly Constantinople, ancient Byzantium, largest city and principal seaport of Turkey.",
+    image: "https://images.unsplash.com/photo-1636537511494-c3e558e0702b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fGlzdGFuYnVsfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
+    },
+
+    {
+    id: 2,
+    name: "Antalya",
+    description: "The Antalya Region, offering all the mysticism of past in our day, is now called the Turkish Riviera due to its archaeological and natural beauties. Antalya is the place where sea, sun, history and nature constitute a perfect harmony and which also includes the most beautiful and clearest coast along the Mediterranean.",
+    image: "https://images.unsplash.com/photo-1592070217898-afe70010c09f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fGFudGFseWF8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60"
+    },
+
+    {
+    id: 3,
+    name: "Izmir",
+    description: "Izmir is a metropolitan city in the western extremity of Turkey and the third most populous city in Turkey, after Istanbul and Izmir. Once the ancient city of Smyrna, İzmir is now a modern, developed, and busy commercial center, set around a huge bay and surrounded by mountains.",
+    image: "https://images.unsplash.com/photo-1606864034826-111e5945de8d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjN8fGJvZHJ1bXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60"
+    },
+
+    {
+    id: 4,
+    name: "Bodrum",
+    description: "An impressive medieval castle built by the Knights of Rhodes guards the entrance to Bodrum's dazzling blue bay, in which the Aegean and the Mediterranean Seas meet. The town's charm is well-known, attracting a diverse population of vacationers who stroll along its long palm-lined waterfront, while elegant yachts crowd the marina.",
+    image: "https://images.unsplash.com/photo-1535916707207-35f97e715e1c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NjJ8fGl6bWlyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
+    },
+
+    {
+    id: 5,
+    name: "Ankara",
+    description: "Ankara is the capital city of Turkey and the country's second largest city after Istanbul. The city has a population (as of 2019) of 5,5 million, and a mean elevation of 850 m (2800 ft). It was formerly known as Angora. The Hittites gave it the name Ankuwash before 1200 BC, the Galatians and Romans called it Ancyra, and in the classical, Hellenistic, and Byzantine periods it was known as Ánkyra. Ankara also serves as the capital of the Province of Ankara.",
+    image: "https://images.unsplash.com/photo-1577353834818-0f8472391b6a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YW5rYXJhfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
+    },
+
+    {
+    id: 6,
+    name: "Urfa",
+    description: "Urfa (also Şanlıurfa, formerly Edessa) is a city in Southeastern Anatolia, and the provincial capital of Şanlıurfa Province. The modern city of Urfa is situated about 80 km east of the Euphrates River. It has a rapidly growing population. Urfa has many excellent old buildings and plenty of connections with the Old Testament and Islamic tradition. The general vibe is absolutely Middle Eastern, with all those traditional yellow stone, arched architecture, people of both genders in traditional Middle Eastern attire, and so on. When approaching from the west, the city certainly feels like the gates of the eastern world. People are extremely friendly, and the bazaar is great.",
+    image: "https://images.unsplash.com/photo-1563388705-c240527afb77?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzZ8fHR1cmtleSUyMGNvdW50cnl8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60"
+    },
+
+    {
+    id: 7,
+    name: "Konya",
+    description: "Konya is a city in Central Anatolia in Turkey which has protected its name for centuries. Legend says that Perseus killed a dragon that had been ravaging the town. The people set up a special monument to honor him, a stone obelisk with an icon of Perseus carved in it. This event gave the city it's name, Ikonyon, Ikonyum, Iconium.",
+    image: "https://images.unsplash.com/photo-1508786301-5d6544666ba2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8dHVya2V5JTIwY291bnRyeXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60"
+    },
+
+    {
+    id: 8,
+    name: "Side",
+    description: "Side, modern Selimiye, principal city and port of ancient Pamphylia, originally situated on the Mediterranean coast west of the mouth of the Manavgat River, in southwestern Turkey. (The site is now inland.) Though the city was founded by Aeolian Greeks, a peculiar non-Greek language was spoken there. Having a good natural harbour and two artificial harbours for larger vessels, it was the most important port in Pamphylia.",
+    image: "https://images.unsplash.com/photo-1513073945753-eb763d2a99de?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dHVya2V5JTIwYmVhY2h8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60"
+    }
+
+]
